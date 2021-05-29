8353f0bb6f161a646e64cc6484fd0c54db6c8845
Nagendra Babu Epuri <56201101+NagendraEpuri@users.noreply.github.com>
2021-05-29 15:21:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,9 +23,16 @@ app.listen(3000, () => console.log("Server Up and running"));
 //view engine configuration
 app.set("view engine", "ejs");
 //get method
-app.get('/',(req, res) => {
-    res.render('todo.ejs');
+//app.get('/',(req, res) => {
+   // res.render('todo.ejs');
+   // });
+// GET METHOD
+app.get("/", (req, res) => {
+    TodoTask.find({}, (err, tasks) => {
+    res.render("todo.ejs", { todoTasks: tasks });
     });
+    });
+
 
 //post method
 //app.post('/',(req, res) => {
@@ -46,3 +53,29 @@ app.post('/',async (req, res) => {
     }
 });
 //app.listen(3000, () => console.log("Server Up and running"));
+
+//UPDATE
+app
+.route("/edit/:id")
+.get((req, res) => {
+const id = req.params.id;
+TodoTask.find({}, (err, tasks) => {
+res.render("todoEdit.ejs", { todoTasks: tasks, idTask: id });
+});
+})
+.post((req, res) => {
+const id = req.params.id;
+TodoTask.findByIdAndUpdate(id, { content: req.body.content }, err => {
+if (err) return res.send(500, err);
+res.redirect("/");
+});
+});
+
+//DELETE
+app.route("/remove/:id").get((req, res) => {
+    const id = req.params.id;
+    TodoTask.findByIdAndRemove(id, err => {
+    if (err) return res.send(500, err);
+    res.redirect("/");
+    });
+    });
